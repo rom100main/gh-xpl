4a5c4b2e0d57112a18e90d3be3abed016cb839c0
rom100main <rom100main@proton.me>
2026-04-23 21:28:03 +0200

    feat: config

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const DEFAULT_SEARCH_COUNT = 10;
+
+export const GITHUB_API_BASE_URL = "https://api.github.com";
+export const GITHUB_RAW_BASE_URL = "https://raw.githubusercontent.com";
+
+export const SKILL_NAME = "explore-github";
